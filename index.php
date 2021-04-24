304dbdc9b63a3550e2b3e37b78c0c751f68df3ed
JackSparrow17 <wubontow@gmail.com>
2021-04-24 07:08:22 -0700

    Uploading files

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title> </title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- External Stylesheets -->
+    <link rel="stylesheet" href="css/main.css" type="text/css">
+
+    <!-- Jquery File -->
+    <script src="JS/Jquery/jquery351.js"></script>
+    <!-- Font Awesome -->
+    <script src="https://use.fontawesome.com/07b6c584d8.js"></script>
+</head>
+
+<body>
+    <h1> Hello World! </h1>
+</body>
+</html>
