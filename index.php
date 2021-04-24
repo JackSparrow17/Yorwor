cfc10647aaa963955f401e52c8e963f24a48fcae
JackSparrow17 <wubontow@gmail.com>
2021-04-24 09:23:04 -0700

    Version 1.2 Added Images

--- index.php
+++ index.php
@@ -7,14 +7,131 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- External Stylesheets -->
     <link rel="stylesheet" href="css/main.css" type="text/css">
+    <link rel="stylesheet" href="css/home.css" type="text/css">
 
     <!-- Jquery File -->
     <script src="JS/Jquery/jquery351.js"></script>
     <!-- Font Awesome -->
     <script src="https://use.fontawesome.com/07b6c584d8.js"></script>
+
+
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
 </head>
 
 <body>
-    <h1> Hello World! </h1>
+    <div class="Container Wide">
+        <!-- Callout -->
+        <div class="Callout Wide" id="SearchArea">
+            <div class="Brand">
+                Brand
+                <a href="#"> Sign In </a>
+                <a href="#"> Register </a>
+            </div>
+
+            <div class="Extras">
+                <span id="Account"><i class="fa fa-user"></i></span>  
+            </div>
+        </div>
+        <!-- Banner -->
+        <div class="Banner Wide">
+            <div class="Inner">
+                <div class="Text">
+                    What are you looking for?
+                </div>
+
+                <div class="Search-Form">
+                    <form>
+                        <input type="text" placeholder="Type your search here..." name="search-word"/>
+                        <button type="submit" name="search-btn"> <i class="fa fa-search"></i> </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+        <!-- Product Categories -->
+        <div class="Categories Wide">
+            <a href="#">
+                <div class="Item">
+                    <p><img src="Images/Icons/car.png" /></p>
+                    <p> Vehicles </p>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="Item">
+                    <p><img src="Images/Icons/blender.png" /></p>
+                    <p> Electronics </p>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="Item">
+                    <p><img src="Images/Icons/smartphone.png" /></p>
+                    <p> Phones & Tablets </p>
+                </div>
+
+            </a>
+            <a href="#">   
+                <div class="Item">
+                    <p><img src="Images/Icons/laptop.png" /></p>
+                    <p> Computers </p>
+                </div>
+            </a>
+            <a href="#">    
+                <div class="Item">
+                    <p><img src="Images/Icons/sofa.png" /></p>
+                    <p> Home & Furniture </p>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="Item">
+                    <p><img src="Images/Icons/dress.png" /></p>
+                    <p> Fashion </p>
+                </div>
+            </a>
+            <a href="#">
+                <div class="Item">
+                    <p><img src="Images/Icons/skincare.png" /></p>
+                    <p> Health & Beauty </p>
+                </div>
+            </a>
+            <a href="#">
+                <div class="Item">
+                    <p><img src="Images/Icons/suitcase.png" /></p>
+                    <p> Jobs </p>
+                </div>
+            </a>
+            <a href="#">
+                <div class="Item">
+                    <p><img src="Images/Icons/fast-food.png" /></p>
+                    <p> Supermarket </p>
+                </div>   
+            </a>
+
+            <a href="#">
+                <div class="Item">
+                    <p><img src="Images/Icons/24-hours.png" /></p>
+                    <p> Services </p>
+                </div>   
+            </a>
+        </div>
+
+        <!-- Recent Uploads -->
+        <div class="Recent-Uploads Wide">
+            Recent
+        </div>
+
+        <!-- Navigation -->
+        <div class="Navigation Wide">
+            <a href="index.php"><i class="fa fa-home"></i></a>
+            <span href="#"><i class="fa fa-star"></i></span>
+            <span class="Primary"><i class="fa fa-shopping-cart"></i></span>
+            <a href="#"><i class="fa fa-search"></i></a>
+            <span><i class="fa fa-user"></i></span>
+        </div>
+    </div>
 </body>
 </html>
