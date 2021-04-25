32aaee05e9925981833d583ef35435cbc5298def
JackSparrow17 <wubontow@gmail.com>
2021-04-25 03:54:49 -0700

    version 2.1

--- viewProducts.php
+++ viewProducts.php
@@ -13,7 +13,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title> </title>
+    <title><?php echo $productName;?> </title>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
